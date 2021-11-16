90fce8030c8a6dbf1725b5a31d585d62e7ed0382
subhadipphloxblog <subhadipphloxblog@gmail.com>
2021-11-16 18:51:07 +0530

    1st chnge

---
+++ form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View, Button,} from 'react-native';
+
+const Registretion = () => {
+  return (
+    <View style={styles.container}>
+     <text>mohit</text>
+    </View>
+  );
+}
+
+export default Registretion;
